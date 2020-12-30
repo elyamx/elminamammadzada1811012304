edf8a6eb7c767ee4a79724dec9fbe733e81ae009
Elmina <49524264+elyamx@users.noreply.github.com>
2020-12-30 21:25:37 +0300

    ilk commit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var createError = require('http-errors');
+require('./app_api/models/db');
 var express = require('express');
-var favicon = require('serve-favicon');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
 
+var routesApi = require('./app_api/routes/index');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 
@@ -21,6 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/api', routesApi);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -41,4 +44,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
